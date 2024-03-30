b6533ee8021c68f25d93e415569d1a5cb0a76e6e
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-03-30 10:06:28 +0700

    [WIP] add registration form

--- app/(login)/register/page.tsx
+++ app/(login)/register/page.tsx
@@ -1,4 +1,71 @@
 "use client";
+
+// import { Button } from "@nextui-org/button";
+// import { Card, CardBody, CardHeader } from "@nextui-org/card";
+// import { Input } from "@nextui-org/input";
+// import { useState } from "react";
+// import { SubmitHandler, useForm } from "react-hook-form";
+
+// interface IRegisterEmail {
+//   email: string;
+// }
+
+// const components = [
+//   ({ next }: { next: () => void }) => {
+//     const {
+//       register,
+//       handleSubmit,
+//       formState: { errors },
+//     } = useForm<IRegisterEmail>({
+//       mode: "onChange",
+//       defaultValues: {
+//         email: "",
+//       },
+//     });
+
+//     const onSubmit: SubmitHandler<IRegisterEmail> = (data) => {};
+
+//     return (
+//       <Card className="flex w-full flex-wrap md:flex-nowrap w-96 px-8 py-4">
+//         <CardHeader className="pb-0 pt-2 flex-col items-start">
+//           <h1 className="text-2xl">Register</h1>
+//         </CardHeader>
+//         <form onSubmit={handleSubmit(onSubmit)}>
+//           <CardBody className="gap-4">
+//             <Input
+//               type="email"
+//               label="Email"
+//               placeholder="Enter your email"
+//               errorMessage={errors.email?.message}
+//               {...register("email", {
+//                 required: "Email is Required",
+//                 pattern: {
+//                   value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
+//                   message: "Invalid email address",
+//                 },
+//               })}
+//             />
+//             <div className="flex-row flex gap-4 justify-between">
+//               <Button className="w-full" type="submit" color="primary">
+//                 Register
+//               </Button>
+//             </div>
+//           </CardBody>
+//         </form>
+//       </Card>
+//     );
+//   },
+//   ({ next }: { next: () => void }) => {},
+// // ];
+
+// export default function RegisterPage() {
+//   const [stage, setStage] = useState(0);
+
+//   const next = () => setStage(stage + 1);
+//   return components[stage]({ next });
+// }
+
+"use client";
 import appService from "@/services/cms";
 import {
   Button,
