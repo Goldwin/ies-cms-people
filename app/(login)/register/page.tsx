c428031c91b6f1113a4ee472ebd1e6f5b7a12425
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-04-01 09:26:18 +0700

    modify registration layout

--- app/(login)/register/page.tsx
+++ app/(login)/register/page.tsx
@@ -1,71 +1,4 @@
 "use client";
-
-// import { Button } from "@nextui-org/button";
-// import { Card, CardBody, CardHeader } from "@nextui-org/card";
-// import { Input } from "@nextui-org/input";
-// import { useState } from "react";
-// import { SubmitHandler, useForm } from "react-hook-form";
-
-// interface IRegisterEmail {
-//   email: string;
-// }
-
-// const components = [
-//   ({ next }: { next: () => void }) => {
-//     const {
-//       register,
-//       handleSubmit,
-//       formState: { errors },
-//     } = useForm<IRegisterEmail>({
-//       mode: "onChange",
-//       defaultValues: {
-//         email: "",
-//       },
-//     });
-
-//     const onSubmit: SubmitHandler<IRegisterEmail> = (data) => {};
-
-//     return (
-//       <Card className="flex w-full flex-wrap md:flex-nowrap w-96 px-8 py-4">
-//         <CardHeader className="pb-0 pt-2 flex-col items-start">
-//           <h1 className="text-2xl">Register</h1>
-//         </CardHeader>
-//         <form onSubmit={handleSubmit(onSubmit)}>
-//           <CardBody className="gap-4">
-//             <Input
-//               type="email"
-//               label="Email"
-//               placeholder="Enter your email"
-//               errorMessage={errors.email?.message}
-//               {...register("email", {
-//                 required: "Email is Required",
-//                 pattern: {
-//                   value: /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/g,
-//                   message: "Invalid email address",
-//                 },
-//               })}
-//             />
-//             <div className="flex-row flex gap-4 justify-between">
-//               <Button className="w-full" type="submit" color="primary">
-//                 Register
-//               </Button>
-//             </div>
-//           </CardBody>
-//         </form>
-//       </Card>
-//     );
-//   },
-//   ({ next }: { next: () => void }) => {},
-// // ];
-
-// export default function RegisterPage() {
-//   const [stage, setStage] = useState(0);
-
-//   const next = () => setStage(stage + 1);
-//   return components[stage]({ next });
-// }
-
-"use client";
 import appService from "@/services/cms";
 import {
   Button,
@@ -77,12 +10,16 @@ import {
   Progress,
 } from "@nextui-org/react";
 import { useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { SubmitHandler, useForm, UseFormRegister } from "react-hook-form";
 
 interface IRegistrationInput {
   email: string;
   otp: string;
-  name: string;
+  firstName: string;
+  lastName: string;
+  middleName: string;
+  password: string;
+  confirmPassword: string;
 }
 
 const EmailConfirmationStage = (
@@ -100,6 +37,7 @@ const EmailConfirmationStage = (
 
   return (
     <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <p className="text-sm">Step 1, Enter your Email</p>
       <Input
         type="email"
         label="Email"
@@ -133,7 +71,8 @@ const OTPConfirmationStage = (
     nextStage();
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <p className="text-sm">Step 2, Enter your OTP</p>
       <Input
         type="otp"
         label="OTP"
@@ -155,25 +94,67 @@ const OTPConfirmationStage = (
 const RegistrationStage = (
   nextStage: () => void,
   handleSubmit: any,
-  register: any,
+  register: UseFormRegister<IRegistrationInput>,
   errors: any
 ): any => {
   console.log("registering");
   const onSubmit: SubmitHandler<IRegistrationInput> = (data) => {
     console.log(data);
-    nextStage();
   };
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
+    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-4">
+      <p className="text-sm">Step 3, Enter your basic information</p>
+      <div className="flex flex-row gap-4">
+        <Input
+          type="text"
+          label="First Name"
+          placeholder="Enter your first name"
+          errorMessage={errors.firstName?.message}
+          {...register("firstName", {
+            required: "Name is Required",
+          })}
+        />
+
+        <Input
+          type="text"
+          label="middle Name"
+          placeholder="Enter your middle name"
+          errorMessage={errors.middleName?.message}
+          {...register("middleName")}
+        />
+
+        <Input
+          type="text"
+          label="Last Name"
+          placeholder="Enter your last name"
+          errorMessage={errors.lastName?.message}
+          {...register("lastName", {
+            required: "Name is Required",
+          })}
+        />
+      </div>
+
       <Input
-        type="info"
-        label="infooo"
-        placeholder="Enter your information"
-        errorMessage={errors.name?.message}
-        {...register("name", {
-          required: "Name is Required",
+        type="password"
+        label="Password"
+        placeholder="Enter your password"
+        errorMessage={errors.password?.message}
+        {...register("password", {
+          required: "Password is Required",
         })}
       />
+
+      <Input
+        type="password"
+        label="Confirm Password"
+        placeholder="Confirm your password"
+        errorMessage={errors.confirmPassword?.message}
+        {...register("confirmPassword", {
+          validate: (value: string, formValue: IRegistrationInput) =>
+            value === formValue.password || "Passwords do not match",
+        })}
+      />
+
       <div className="flex-row flex gap-4 justify-between">
         <Button className="w-full" type="submit" color="primary">
           Next
@@ -207,9 +188,12 @@ export default function RegisterPage() {
 
   const nextStage = () => setStage(stage + 1);
   return (
-    <Card className="flex w-full flex-wrap md:flex-nowrap w-96 px-8 py-4 justify-center">
+    <Card
+      className="flex w-full flex-wrap md:flex-nowrap w-[48rem] mx-[-8rem] px-8 py-4 justify-center"
+      fullWidth={true}
+    >
       <CardHeader className="pb-0 pt-2 flex-col items-start">
-        <h1 className="text-2xl">Register</h1>
+        <h1 className="text-2xl">Registration</h1>
       </CardHeader>
 
       <CardBody className="gap-4">
