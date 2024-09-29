aa6e2dba9173ad1351187178b4472c7ad34b01a5
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-09-29 23:21:40 +0700

    dynamic rendering apex chart

--- app/(management)/attendance/page.tsx
+++ app/(management)/attendance/page.tsx
@@ -18,7 +18,9 @@ import { Skeleton, Tab, Tabs, useDisclosure } from "@nextui-org/react";
 import _ from "lodash";
 import { useTheme } from "next-themes";
 import { useEffect, useState } from "react";
-import Chart from "react-apexcharts";
+import dynamic from "next/dynamic";
+
+const Chart = dynamic(() => import("react-apexcharts"), { ssr: false });
 
 const ChurchEventAction = ({ churchEvent }: { churchEvent: EventSchedule }) => {
   return (
