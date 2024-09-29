0fb2de1c6509dea0fa90603a536b90d61edc661b
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-09-30 03:05:50 +0700

    inject env to nextjs config

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const webpack = require("webpack");
 const { parsed: myEnv, error: envError } = require("dotenv").config();
 
 const nextConfig = {
+  env: {
+    AUTH_URL: process.env.AUTH_URL,
+    PEOPLE_URL: process.env.PEOPLE_URL,
+    APP_URL: process.env.APP_URL,
+    ATTENDANCE_URL: process.env.ATTENDANCE_URL,
+  },
   webpack(config) {
     config.plugins.push(new webpack.EnvironmentPlugin(myEnv));
     return config;
