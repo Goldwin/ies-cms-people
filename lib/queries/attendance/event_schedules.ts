6ba67eb145bbf256d507ae97d4ac92bf880d37bf
Hutomo Widjaja <flame.rainHbow@gmail.com>
2024-11-17 13:39:18 +0700

    remove unused mock class

--- lib/queries/attendance/event_schedules.ts
+++ lib/queries/attendance/event_schedules.ts
@@ -32,54 +32,5 @@ class APIEventScheduleQuery implements EventScheduleQuery {
   }
 }
 
-class MockEventScheduleQuery implements EventScheduleQuery {
-  getEventSchedule(eventScheduleId: string): Promise<EventSchedule> {
-    return Promise.resolve(
-      new WeeklyEventSchedule({
-        id: "1",
-        name: "test",
-        days: [1, 2, 3, 4, 5, 6, 7],
-        timezoneOffset: 7,
-        activities: [
-          new Activity({
-            id: "1",
-            name: "Adult Service",
-            scheduleId: "1",
-            timeHour: 9,
-            timeMinute: 30,
-            timezoneOffset: 7,
-          }),
-          new Activity({
-            id: "2",
-            name: "Sunday School",
-            scheduleId: "2",
-            timeHour: 9,
-            timeMinute: 30,
-            timezoneOffset: 7,
-          }),
-        ],
-      })
-    );
-  }
-  listEventSchedules(lastId: string, limit: number): Promise<EventSchedule[]> {
-    return Promise.resolve([
-      new WeeklyEventSchedule({
-        id: "1",
-        name: "test",
-        days: [1, 2, 3, 4, 5, 6, 7],
-        activities: [],
-        timezoneOffset: 7,
-      }),
-      new EventSchedule({
-        id: "2",
-        name: "test2",
-        type: EventScheduleType.Daily,
-        activities: [],
-        timezoneOffset: 7,
-      }),
-    ]);
-  }
-}
-
 export const eventSchedulesQuery: EventScheduleQuery =
   new APIEventScheduleQuery(attendanceService);
